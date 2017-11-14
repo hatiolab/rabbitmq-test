a34b83983ef170c853fa92fff0cbea12602d5d31
root <root@mq.hatiolab.com>
2017-11-14 07:01:25 +0000

    stomp test

--- publisher.js
+++ publisher.js
@@ -10,14 +10,33 @@ amqp.connect('amqp://hatiolab:hatiolab@mq.hatiolab.com', function(err, conn) {
 
   conn.createChannel(function (err, ch) {
     var ex = 'amq.topic';
+    var count = 1;
 
     ch.assertExchange(ex, 'topic', { durable: true });
 
     setInterval(() => {
       let x = Math.random() * 500 + 200;
       let y = Math.random() * 500 + 200;
-      ch.publish(ex, 'location', new Buffer('{"x":' + x + ', "y":' + y + '}'));
+
+      let location = {x, y}
+      count++;
+      ch.publish(ex, 'movement', new Buffer(JSON.stringify({location, count})));
+    }, 550);
+
+    setInterval(() => {
+      let rotation = Math.random() * Math.PI * 2;
+      ch.publish(ex, 'movement', new Buffer(JSON.stringify({rotation})));
     }, 500);
+
+    setInterval(() => {
+      let color = {
+        r: Math.floor(Math.random() * 255),
+        g: Math.floor(Math.random() * 255),
+        b: Math.floor(Math.random() * 255)
+      }
+
+      ch.publish(ex, 'movement', new Buffer(JSON.stringify({color})));
+    }, 450);
   });
 });
 
--- sender.js
+++ sender.js
@@ -9,16 +9,34 @@ amqp.connect('amqp://hatiolab:hatiolab@mq.hatiolab.com', function(err, conn) {
   }
 
   conn.createChannel(function (err, ch) {
-    var q = 'hello';
+    var q = 'movement';
+    var count = 1;
 
-    ch.assertQueue(q, { durable: false });
+    ch.assertQueue(q, { durable: true });
 
-    for(var i = 0;i < 10000;i++) {
-        ch.sendToQueue(q, new Buffer('Hello World!'));
-    }
-    console.log(" [x] Sent 'Hello World!'");
+    setInterval(() => {
+      let x = Math.random() * 500 + 700;
+      let y = Math.random() * 500 + 200;
+
+      let location = {x, y}
+      count++;
+      ch.sendToQueue(q, new Buffer(JSON.stringify({location, count})));
+    }, 550);
+
+    setInterval(() => {
+      let rotation = Math.random() * Math.PI * 2;
+      ch.sendToQueue(q, new Buffer(JSON.stringify({rotation})));
+    }, 500);
+
+    setInterval(() => {
+      let color = {
+        r: Math.floor(Math.random() * 255),
+        g: Math.floor(Math.random() * 255),
+        b: Math.floor(Math.random() * 255)
+      }
+
+      ch.sendToQueue(q, new Buffer(JSON.stringify({color})));
+    }, 490);
   });
-
-  setTimeout(function() { conn.close(); process.exit(0) }, 500000);  
 });
 
