b1fa7bfd45951c2229c65bf444033f43db8dd2e6
Hearty Oh <heartyoh@hatiolab.com>
2017-11-16 09:33:31 +0800

    publish table

---
+++ publisher-table.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://hatiolab:hatiolab@mq.hatiolab.com', function(err, conn) {
+  if(err) {
+    console.error(err);
+    return;
+  }
+
+  conn.createChannel(function (err, ch) {
+    var ex = 'amq.topic';
+    var count = 1;
+
+    ch.assertExchange(ex, 'topic', { durable: true });
+
+    setInterval(() => {
+      let table = []
+      for(let i = 0;i < 4;i++) {
+        table.push({
+          qoodQty: Math.floor(Math.random() * 500),
+          badQty: Math.floor(Math.random() * 50)
+        })
+      }
+
+      ch.publish(ex, 'table', new Buffer(JSON.stringify(table)));
+    }, 5000);
+  });
+});
+
